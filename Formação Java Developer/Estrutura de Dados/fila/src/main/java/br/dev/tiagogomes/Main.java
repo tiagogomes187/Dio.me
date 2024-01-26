0320985be5c51adf721c5ab52769f6db9128f304
Tiago Gomes <tiagogomes187@gmail.com>
2024-01-25 23:36:36 -0300

    Estruturas de dados, Filas e Pilhas

---
+++ Formação Java Developer/Estrutura de Dados/fila/src/main/java/br/dev/tiagogomes/Main.java
@@ -0,0 +1,21 @@
+package br.dev.tiagogomes;
+
+public class Main {
+	public static void main(String[] args) {
+		Fila<String> minhaFila = new Fila<String>();
+
+		minhaFila.enqueue("primeiro");
+		minhaFila.enqueue("segundo");
+		minhaFila.enqueue("terceiro");
+		minhaFila.enqueue("quarto");
+
+
+		System.out.println(minhaFila);
+		System.out.println(minhaFila.dequeue());
+		System.out.println(minhaFila);
+		minhaFila.enqueue("ultimo");
+		System.out.println(minhaFila);
+		System.out.println(minhaFila.first());
+		System.out.println(minhaFila);
+	}
+}
