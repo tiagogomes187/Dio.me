71dd3e5177d5208978ca20615829c3c3f4e2ec38
Tiago Gomes <tiagogomes187@gmail.com>
2024-03-14 21:59:48 -0300

    Praticando com Java Puro: Facade

---
+++ Formação Java Developer/Explorando Padrões de Projetos na Prática com Java/lab-padroes-projeto-java/src/br/dev/tiagogomes/gof/facade/Test.java
@@ -0,0 +1,10 @@
+package br.dev.tiagogomes.gof.facade;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Facade facade = new Facade();
+		facade.migrarCliente("Tiago", "14548666");
+	}
+}
