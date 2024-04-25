8a34bfc166925a3d1db719608b2ccbe96ed9da5b
Tiago Gomes <tiagogomes187@gmail.com>
2024-04-24 21:09:51 -0300

    Desafio 'VerificacaoChequeEspecial' implementado e funcionando.

---
+++ 1 - Santander Bootcamp 2024 - Backend Java/Desafios de Códigos/verificacaochequeespecial/src/br/dev/tiagogomes/dio/VerificacaoChequeEspecial.java
@@ -0,0 +1,23 @@
+package br.dev.tiagogomes.dio;
+
+import java.util.Scanner;
+
+public class VerificacaoChequeEspecial {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		double saldo = scanner.nextDouble();
+		double saque = scanner.nextDouble();
+		double limiteChequeEspecial = 500;
+
+		if (saque <= saldo) {
+			System.out.println("Transacao realizada com sucesso.");
+		} else if (saque > saldo && saque <= (saldo + limiteChequeEspecial)) {
+			System.out.println("Transacao realizada com sucesso utilizando o cheque especial.");
+		} else if (saque > (saldo + limiteChequeEspecial)) {
+			System.out.println("Transacao nao realizada. Limite do cheque especial excedido.");
+		}
+
+		scanner.close();
+	}
+}
