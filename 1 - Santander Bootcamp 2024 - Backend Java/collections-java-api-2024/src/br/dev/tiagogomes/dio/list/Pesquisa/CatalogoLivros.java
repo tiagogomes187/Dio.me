2946de3328332fd97a0d1801750c4f01f3977d2f
Tiago Gomes <tiagogomes187@gmail.com>
2024-05-08 00:10:32 -0300

    Pesquisa List: Testando métodos

--- 1 - Santander Bootcamp 2024 - Backend Java/collections-java-api-2024/src/br/dev/tiagogomes/dio/list/Pesquisa/CatalogoLivros.java
+++ 1 - Santander Bootcamp 2024 - Backend Java/collections-java-api-2024/src/br/dev/tiagogomes/dio/list/Pesquisa/CatalogoLivros.java
@@ -39,17 +39,31 @@ public class CatalogoLivros {
 		return livrosPorIntervaloAnos;
 	}
 
-	public Livro pesquisarPorTitulo (String titulo){
+	public Livro pesquisarPorTitulo(String titulo) {
 		Livro livroPorTitulo = null;
-		if (!livroList.isEmpty()){
-			for(Livro l : livroList){
-				if (l.getTitulo().equalsIgnoreCase(titulo)){
+		if (!livroList.isEmpty()) {
+			for (Livro l : livroList) {
+				if (l.getTitulo().equalsIgnoreCase(titulo)) {
 					livroPorTitulo = l;
 					break;
 				}
 			}
 		}
 		return livroPorTitulo;
+	}
+
+	public static void main(String[] args) {
+		CatalogoLivros catalogoLivros = new CatalogoLivros();
+		catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
+		catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2021);
+		catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2022);
+		catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2023);
+		catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 1994);
+
+		System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+		System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2020, 2022));
+		System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 4"));
+
 	}
 
 }
