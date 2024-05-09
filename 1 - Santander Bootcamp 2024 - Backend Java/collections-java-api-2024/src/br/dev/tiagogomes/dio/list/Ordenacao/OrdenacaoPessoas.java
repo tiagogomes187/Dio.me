594eb5616c8be469989079c484077117777a6a21
Tiago Gomes <tiagogomes187@gmail.com>
2024-05-08 22:21:34 -0300

    Ordenação List: testando os métodos

--- 1 - Santander Bootcamp 2024 - Backend Java/collections-java-api-2024/src/br/dev/tiagogomes/dio/list/Ordenacao/OrdenacaoPessoas.java
+++ 1 - Santander Bootcamp 2024 - Backend Java/collections-java-api-2024/src/br/dev/tiagogomes/dio/list/Ordenacao/OrdenacaoPessoas.java
@@ -27,4 +27,25 @@ public class OrdenacaoPessoas {
 		Collections.sort(pessoasPorAltra, new ComparatorPorAltura());
 		return pessoasPorAltra;
 	}
+
+	public static void main(String[] args) {
+		// Criando uma instância da classe OrdenacaoPessoas
+		OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+
+		// Adicionando pessoas à lista
+		ordenacaoPessoas.adicionarPessoa("Alice", 20, 1.56);
+		ordenacaoPessoas.adicionarPessoa("Bob", 30, 1.80);
+		ordenacaoPessoas.adicionarPessoa("Charlie", 25, 1.70);
+		ordenacaoPessoas.adicionarPessoa("David", 17, 1.56);
+
+		// Exibindo a lista de pessoas adicionadas
+		System.out.println("Sem Ordenar: " + ordenacaoPessoas.pessoaList);
+
+		// Ordenando e exibindo por idade
+		System.out.println("Ordena por Idade: " + ordenacaoPessoas.ordenarPorIdade());
+
+		// Ordenando e exibindo por altura
+		System.out.println("Ordena por Altura: " + ordenacaoPessoas.ordenarPorAltura());
+	}
+
 }
