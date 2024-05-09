36cfca6bcf873e66268006011e851dc8715c6f28
Tiago Gomes <tiagogomes187@gmail.com>
2024-05-08 22:01:52 -0300

    Ordenação List

--- 1 - Santander Bootcamp 2024 - Backend Java/collections-java-api-2024/src/br/dev/tiagogomes/dio/list/Ordenacao/OrdenacaoPessoas.java
+++ 1 - Santander Bootcamp 2024 - Backend Java/collections-java-api-2024/src/br/dev/tiagogomes/dio/list/Ordenacao/OrdenacaoPessoas.java
@@ -1,4 +1,30 @@
 package br.dev.tiagogomes.dio.list.Ordenacao;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class OrdenacaoPessoas {
+
+	private List<Pessoa> pessoaList;
+
+	public OrdenacaoPessoas() {
+		this.pessoaList = new ArrayList<>();
+	}
+
+	public void adicionarPessoa(String nome, int idade, double altura) {
+		pessoaList.add(new Pessoa(nome, idade, altura));
+	}
+
+	public List<Pessoa> ordenarPorIdade() {
+		List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+		Collections.sort(pessoasPorIdade);
+		return pessoasPorIdade;
+	}
+
+	public List<Pessoa> ordenarPorAltura() {
+		List<Pessoa> pessoasPorAltra = new ArrayList<>(pessoaList);
+		Collections.sort(pessoasPorAltra, new ComparatorPorAltura());
+		return pessoasPorAltra;
+	}
 }
